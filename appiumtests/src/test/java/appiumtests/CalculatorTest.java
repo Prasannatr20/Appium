280e63d8946d60c51bce6c5e3a04224ae3eb1590
Prasannatr20 <prasannavenkatesh402@gmail.com>
2026-08-11 06:52:34 +0530

    Addition

--- appiumtests/src/test/java/appiumtests/CalculatorTest.java
+++ appiumtests/src/test/java/appiumtests/CalculatorTest.java
@@ -2,14 +2,18 @@ package appiumtests;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 public class CalculatorTest {
 
-    public static void main(String[] args) throws MalformedURLException {
-
+    public static void main(String[] args) throws MalformedURLException
+    {
         URL url = new URL("http://127.0.0.1:4723/");
 
         UiAutomator2Options options = new UiAutomator2Options();
@@ -23,8 +27,17 @@ public class CalculatorTest {
         options.setAppActivity("com.android.calculator2.Calculator");
 
         AndroidDriver driver = new AndroidDriver(url, options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
         System.out.println("Calculator launched successfully!");
+        
+        
+        
+        driver.findElement(By.id("com.coloros.calculator:id/digit_1")).click();
+        driver.findElement(By.id("com.coloros.calculator:id/digit_5")).click();
+        driver.findElement(By.id("com.coloros.calculator:id/op_add")).click();
+        driver.findElement(By.id("com.coloros.calculator:id/digit_8")).click();
+        driver.findElement(By.id("com.coloros.calculator:id/eq")).click();
 
         driver.quit();
     }
